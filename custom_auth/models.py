d3c1c326f169d987c1041f61b72ac9ab1cc7c179
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-08 14:34:15 +0500

    create cutomuser

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from idlelib.autocomplete import TRY_A
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('Phone number must be set')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('The user must be is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('The user must be is_superuser=True')
+        return self.create_user(phone_number, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    phone_number = models.CharField(max_length=13, unique=True, null=True)
